fc0321165dec42701f38727e163c16298ed67eb8
Kätlin Kilgi <katlin.kilgi@gmail.com>
2013-02-19 12:27:45 +0200

    index

--- katlin/index.php
+++ katlin/index.php
@@ -1,28 +1,31 @@
+<?php
+error_reporting(0);
+session_start();
+/*$arv = rand(1,25);
+$_SESSION['arv']=$arv;*/
+$arv=$_SESSION['arv'];
+//echo $_SESSION['arv'];
+?>
+
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     Sisesta arv: <input type="int" name="a"><br />
     <input type="submit" value="Arva!"><br />
 </form>
 
 <?php
-error_reporting(0);
-
-session_start();
-$_SESSION['arv']=15;
-
-//echo $_SESSION['rand'];
-
-
-if ($_POST["a"]>$_SESSION['arv'])
-{
-    echo "Tegelik number on vaiksem";
-}
-if ($_POST["a"]<$_SESSION['arv'])
-{
-    echo "Tegelik number on suurem";
-}
-if ($_POST["a"]==$_SESSION['arv'])
-{
-    echo "Palju onne!";
+if (isset($_POST["a"])){
+    if ($_POST["a"]>$_SESSION['arv'])
+    {
+        echo "Tegelik number on vaiksem";
+    }
+    if ($_POST["a"]<$_SESSION['arv'])
+    {
+        echo "Tegelik number on suurem";
+    }
+    if ($_POST["a"]==$_SESSION['arv'])
+    {
+        echo "Palju onne!";
+    }
 }
 
 ?>
